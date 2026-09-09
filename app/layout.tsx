0e823db6f590667215215bbc957c9eb4215cac30
armansyahhkm1995 <armansyahhakim263@gmail.com>
2026-09-09 11:39:37 +0700

    pasang code tracking microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { SmoothScroll } from "@/components/primitives/SmoothScroll";
 import {
   Instrument_Serif,
@@ -48,6 +49,16 @@ export default function RootLayout({
       >
         <SmoothScroll>{children}</SmoothScroll>
       </body>
+
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "yffsfx3ncz");
+        `}
+      </Script>
     </html>
   );
 }
